8e95a2b0a1b19387e8daa0587b205735902a9809
pavan850190 <pavanraj9934@gmail.com>
2022-02-28 23:23:03 +0530

    CODE

--- Day_01/day1.java
+++ Day_01/day1.java
@@ -1,5 +1,5 @@
 // day 1 code of cricket match
-//
+
 import java.util.*;
 import java.io.*;
 import java.util.Scanner;
@@ -33,3 +33,44 @@ public class day1{
     }
 
 }
+/* # QUESTION
+Consider you guys have to develop the Cricket match result application, based on the Innings information. The following details given
+Data type   Field name
+String      teamname
+String      inningsname
+int         runs
+The requirements are the following:
+To Secure the above data
+To assign and access the data indirectly.
+Based on the innings name the condition given below
+If inningsname is “first” –  to display the result like “runs+1” to Win the match
+If inningsname is “second” -  to display the result like “Match Ended”.
+Any other than this “Invalid Input”.
+Test it using the Main Class.
+
+Sample Input 1:
+Enter the team name:
+Australia
+Enter session:
+First
+Enter runs:
+300
+
+Sample output 1
+Name: Australia
+Scored: 300
+Need 301 to win
+
+Sample Input 2:
+Enter the team name:
+Australia
+Enter session:
+Second
+Enter runs:
+200
+
+Sample output 2
+Name: Australia
+Scored: 200
+Match Ended.
+*/
