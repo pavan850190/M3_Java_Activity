4810f59f6ff048619bda341a9fa5e3efad227f66
pavan850190 <68550769+pavan850190@users.noreply.github.com>
2022-02-28 23:53:47 +0530

    Update day1.java

--- Day_01/day1.java
+++ Day_01/day1.java
@@ -4,7 +4,7 @@ import java.util.*;
 import java.io.*;
 import java.util.Scanner;
 
-import javax.lang.model.util.ElementScanner6;
+
 public class day1{
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
